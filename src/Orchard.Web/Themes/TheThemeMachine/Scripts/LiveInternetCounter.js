7f80a43e06869ceff4125f941c0dedf1ab75a707
Nikulin Dmitriy <dmytro.nikulin@gmail.com>
2012-11-05 16:47:39 +0200

    Added LiveInet counter

---
+++ src/Orchard.Web/Themes/TheThemeMachine/Scripts/LiveInternetCounter.js
@@ -0,0 +1,11 @@
+﻿<!--LiveInternet counter--><script type="text/javascript"><!--
+document.write("<a href='http://www.liveinternet.ru/click' "+
+"target=_blank><img src='//counter.yadro.ru/hit?t28.6;r"+
+escape(document.referrer)+((typeof(screen)=="undefined")?"":
+";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+";h"+escape(document.title.substring(0,80))+";"+Math.random()+
+"' alt='' title='LiveInternet: показано количество просмотров и"+
+" посетителей' "+
+"border='0' width='88' height='120'><\/a>")
+//--></script><!--/LiveInternet-->
