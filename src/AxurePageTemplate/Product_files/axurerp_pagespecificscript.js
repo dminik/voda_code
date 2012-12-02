bbafa5e1beac1ebfdbac22cfd1af888769c57b1f
Nikulin Dmitriy <dmytro.nikulin@gmail.com>
2012-12-02 02:22:13 +0200

    Added Axure Product page template.

---
+++ src/AxurePageTemplate/Product_files/axurerp_pagespecificscript.js
@@ -0,0 +1,48 @@
+﻿for(var i = 0; i < 19; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+if (true) {
+
+SetWidgetSelected('u13');
+}
+
+});
+gv_vAlignTable['u16'] = 'center';document.getElementById('u15_img').tabIndex = 0;
+
+u15.style.cursor = 'pointer';
+$axure.eventManager.click('u15', function(e) {
+
+if (true) {
+
+SetWidgetSelected('u15');
+	SetPanelState('u0', 'pd1u0','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u4'] = 'center';gv_vAlignTable['u8'] = 'center';gv_vAlignTable['u10'] = 'top';document.getElementById('u17_img').tabIndex = 0;
+
+u17.style.cursor = 'pointer';
+$axure.eventManager.click('u17', function(e) {
+
+if (true) {
+
+SetWidgetSelected('u17');
+	SetPanelState('u0', 'pd2u0','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u9'] = 'top';gv_vAlignTable['u14'] = 'center';gv_vAlignTable['u6'] = 'center';gv_vAlignTable['u2'] = 'center';gv_vAlignTable['u11'] = 'top';document.getElementById('u13_img').tabIndex = 0;
+
+u13.style.cursor = 'pointer';
+$axure.eventManager.click('u13', function(e) {
+
+if (true) {
+
+SetWidgetSelected('u13');
+	SetPanelState('u0', 'pd0u0','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u18'] = 'center';
