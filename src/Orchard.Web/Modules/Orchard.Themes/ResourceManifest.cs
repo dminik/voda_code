ef660f1f71ec841b2af120f036534b13f29cebe9
Nikulin Dmitriy <dmytro.nikulin@gmail.com>
2012-12-10 21:10:53 +0200

    Tabs was added to Product page.

--- src/Orchard.Web/Modules/Orchard.Themes/ResourceManifest.cs
+++ src/Orchard.Web/Modules/Orchard.Themes/ResourceManifest.cs
@@ -5,6 +5,9 @@ namespace Orchard.Themes {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
             manifest.DefineStyle("ThemesAdmin").SetUrl("orchard-themes-admin.css");
+
+			manifest.DefineScript("jQuery_metadata").SetUrl("jquery.metadata.js").SetDependencies("jQuery");
+			manifest.DefineScript("mbTabset").SetUrl("mbTabset.min.js", "mbTabset.js").SetDependencies("jQuery", "jQueryUI_Core", "jQueryUI_Sortable", "jQuery_metadata");
         }
     }
 }
